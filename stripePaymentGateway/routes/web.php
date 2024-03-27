e212087c19b009461024e572de5a94ab508bee7d
Arti singh <artisingh2724@gmail.com>
2024-03-27 03:30:42 -0700

    stripe payment gateway

--- stripePaymentGateway/routes/web.php
+++ stripePaymentGateway/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StripeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('stripe',[StripeController::class,'stripe'])->name('stripe');
+Route::get('success',[StripeController::class,'success'])->name('success');
+Route::get('cancel',[StripeController::class,'success'])->name('cancel');
